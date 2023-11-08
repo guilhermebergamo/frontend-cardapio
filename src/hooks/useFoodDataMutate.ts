71e5646e810fdb87b9791ac1fd6123135472ccd8
GUILHERME VICENTE BERGAMO ALBINO <g.albino@senff1.onmicrosoft.com>
2023-11-08 13:12:25 -0300

    first commit

---
+++ src/hooks/useFoodDataMutate.ts
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+//import axios, { AxiosPromise } from "axios"
+import { FoodData } from '../interface/FoodData';
+
+const API_URL = 'http://localhost:8080';
+
+const postData = async (data: FoodData): Promise<any> => {
+    //const response = axios.post(API_URL + '/Cardapio', data);
+    const response = await fetch(API_URL + '/Cardapio', {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    },
+    body: JSON.stringify(data)}); 
+    return response;
+}
+
+export function useFoodDataMutate(){
+    const queryClient = useQueryClient();
+    const mutate = useMutation({
+        mutationFn: postData,
+        retry: 2,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['food-data'])
+        }
+    })
+
+    return mutate;
+}
